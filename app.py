1bc3871645bda3e16930b5bad337d51f67c4312f
kumarbibek42 <kumarbibek42@gmail.com>
2020-05-27 15:25:29 +0530

    disable cells and selectable cells

--- app.py
+++ app.py
@@ -10,8 +10,11 @@ class App:
         pygame.display.set_caption(APP_TITLE)
         self.window = pygame.display.set_mode((HIGHT, WIDTH))
         self.running = True
+        self.gridData = grid_num_array
         self.mousePos = None
         self.selected_box = None
+        self.font = pygame.font.SysFont("arial", COLUMNSIZE//2)
+        self.disabledCells = set()
 
     def run(self):
         while self.running:
@@ -26,9 +29,10 @@ class App:
 
     def drawing(self):
         self.window.fill(WHITE)
-        self.drawborder()
         if self.selected_box:
             self.draw_selected_box(self.selected_box)
+        self.draw_numbers_to_the_grid()
+        self.drawborder()
         pygame.display.update()
 
     def eventhandling(self):
@@ -39,11 +43,11 @@ class App:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 print(self.mousePos)
                 if self.is_mouse_position_inside_grid(self.mousePos):
-                    # print('Inside Grid')
-                    self.selected_box = self.grid_index_based_on_selected_position(self.mousePos)
-                    # print(self.selected_box[0])
+                    selected_cell = self.grid_index_based_on_selected_position(self.mousePos)
+                    if self.is_editable_cell(selected_cell):
+                        self.selected_box = self.grid_index_based_on_selected_position(self.mousePos)
+                        print("Empty Cell")
                 else:
-                    # print('Outside Grid')
                     self.selected_box = None
 
     def drawborder(self):
@@ -76,10 +80,41 @@ class App:
         return (x_index, y_index)
 
     def draw_selected_box(self, selected_box):
-        rect = self.selected_box_rect(selected_box)
+        rect = self.get_selected_box_rect(selected_box)
         pygame.draw.rect(self.window, BLUE, rect)
 
-    def selected_box_rect(self, selected_box):
+    def draw_disabled_box(self, selected_box):
+        rect = self.get_selected_box_rect(selected_box)
+        pygame.draw.rect(self.window, GRAY, rect)
+
+    def get_selected_box_rect(self, selected_box):
         x_cord = SIDE_MARGIN + selected_box[0]*ROWSIZE
         y_cord = TOP_MARGIN + selected_box[1]*COLUMNSIZE
         return pygame.Rect(x_cord, y_cord, ROWSIZE, COLUMNSIZE)
+
+    def add_text_content(self, window, text, pos):
+        font = self.font.render(text, False, BLACK)
+        font_width = font.get_width()
+        font_height = font.get_height()
+        pos = ((pos[0] + (COLUMNSIZE - font_width)//2), (pos[1] + (COLUMNSIZE - font_height)//2))
+        window.blit(font, pos)
+
+    def get_text_position(self, pos_index):
+        x_pos = pos_index[1]*COLUMNSIZE + TOP_MARGIN
+        y_pos = pos_index[0]*COLUMNSIZE + TOP_MARGIN
+        return (x_pos, y_pos)
+
+    def draw_numbers_to_the_grid(self):
+        for i, row in enumerate(self.gridData):
+            for j, cellValue in enumerate(row):
+                if cellValue != 0:
+                    self. draw_disabled_box((j, i))
+                    self.add_text_content(self.window, str(cellValue), self.get_text_position((i, j)))
+                    blockedCellKey = str(i) + str(j)
+                    self.disabledCells.add(blockedCellKey)
+
+    def is_editable_cell(self, selected_cell):
+        cellKey = str(selected_cell[1])+str(selected_cell[0])
+        if (cellKey in self.disabledCells):
+            return False
+        return True
--- config.py
+++ config.py
@@ -1,9 +1,12 @@
+import random
+
 HIGHT = 600
 WIDTH = 600
 APP_TITLE = 'Sudoko Solver'
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 BLUE = (50, 111, 168)
+GRAY = (62, 64, 66)
 TOP_MARGIN = 12
 SIDE_MARGIN = 12
 THICKLINEWIDTH = 3
@@ -13,3 +16,11 @@ MIN_GRID_X_POSITION = SIDE_MARGIN
 MAX_GRID_X_POSITION = WIDTH - SIDE_MARGIN
 MIN_GRID_Y_POSITION = TOP_MARGIN
 MAX_GRID_Y_POSITION = HIGHT - TOP_MARGIN
+grid_num_array = []
+sudoko_size = 3*3
+for i in range(sudoko_size):
+    rows = []
+    for j in range(sudoko_size):
+        rows.append(random.randint(0, 9))
+    grid_num_array.append(rows)
+print(grid_num_array)
